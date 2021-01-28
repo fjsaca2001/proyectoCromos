7e70cb4e880cae8a160f27d56350f299cbf3286f
SantiagoDGarcia <santiagodaivid@gmail.com>
2021-01-28 13:46:49 -0500

    subidax10

--- EconoCromos/resources/views/internas/actividades.blade.php
+++ EconoCromos/resources/views/internas/actividades.blade.php
@@ -5,7 +5,7 @@
 <link href="{{ asset('css/actividades.css') }}" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<!-- Contenido de la página -->
+<!-- Contenido de la páginas -->
 <section class="contenidoActividades">
     <section class="tablaAlbumes table-responsive">
         @php
